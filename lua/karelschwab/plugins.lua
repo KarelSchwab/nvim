d3fb42fdad69928b15cceeb1914d6052243f174e
Karel Schwab <schwabk9@protonmail.com>
2022-09-24 21:52:04 +0200

    feat: add markdown preview plugin

--- lua/karelschwab/plugins.lua
+++ lua/karelschwab/plugins.lua
@@ -77,5 +77,8 @@ require("packer").startup(
 
         -- Lspsaga
         use "glepnir/lspsaga.nvim"
+
+        -- Markdown preview
+        use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
     end
 )
