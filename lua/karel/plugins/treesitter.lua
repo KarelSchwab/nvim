4067509baa350a737de9153fa867b9b1bec917d9
Karel Schwab <schwabk9@protonmail.com>
2024-01-14 19:23:05 +0200

    Add treesitter

---
+++ lua/karel/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function ()
+        require "nvim-treesitter.configs".setup {
+            ensure_installed = "all",
+            highlight = {
+                enable = true
+            },
+            indent = {
+                enable = false
+            }
+        }
+    end
+}
