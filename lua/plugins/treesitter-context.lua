94191da3fe87e9774071687cdd83bc3e00608fcb
Karel Schwab <karels@shareforce.net>
2025-06-09 08:41:19 +0200

    Update TS configs

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,3 +1,8 @@
 return {
-    "nvim-treesitter/nvim-treesitter-context"
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+        enable              = true,
+    },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
+    build = ":TSUpdate",
+    branch = "master",
+    lazy = false,
     config = function ()
         require "nvim-treesitter.configs".setup {
             ensure_installed = "all",
