9c0c88bc1017af4ad526f22f81457030b4497cf0
Karel Schwab <karels@shareforce.net>
2025-08-05 16:16:18 +0200

    Remove comments

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,15 +2,10 @@ return {
     'stevearc/conform.nvim',
     opts = {
         formatters_by_ft = {
-            -- run Black (and only Black) on Python files.  You can prepend "isort"
-            -- here if you also want your imports sorted.
             python = { "black" },
         },
         formatters = {
             black = {
-                -- `inherit = true` (default) keeps the built-in args:
-                -- { "--quiet", "-", "--stdin-filename", "$FILENAME" }
-                -- We just tack on --line-length 120 in front of them.
                 prepend_args = { "--line-length", "120", "--target-version", "py310" },
             },
         },
