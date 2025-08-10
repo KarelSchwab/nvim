95d889364a5709da0ea08e3cdafa59a792a7050b
Karel Schwab <karels@shareforce.net>
2025-08-10 18:03:46 +0200

    Update conform plugin

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,12 +2,22 @@ return {
     'stevearc/conform.nvim',
     opts = {
         formatters_by_ft = {
-            python = { "black" },
+            python = function(bufnr)
+                if require("conform").get_formatter_info("ruff_format", bufnr).available then
+                    return { "ruff_format" }
+                else
+                    return { "black" }
+                end
+            end,
         },
         formatters = {
             black = {
                 prepend_args = { "--line-length", "120", "--target-version", "py310" },
             },
         },
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        notify_on_error = true,
     },
 }
