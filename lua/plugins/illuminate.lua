3ca1a04c3d3b556dabc4e6c9baaf455d32a4ad3a
Karel Schwab <karels@shareforce.net>
2025-09-24 14:18:31 +0200

    Gruvbox

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,6 +1,6 @@
 return {
     "RRethy/vim-illuminate",
-    config = function ()
+    config = function()
         require("illuminate").configure({
             providers = {
                 'lsp',
